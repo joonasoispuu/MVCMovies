ce06aa6d0a95dcfe600989b5afb3996f24a35560
joonas.oispuu <oispuu.joonas@gmail.com>
2021-11-16 19:46:08 +0200

    Add project files.

---
+++ MvcMovie/Models/ActorSkillViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMovie.Models
+{
+    public class ActorSkillViewModel
+    {
+        public List<Actors> actors { get; set; }
+        public SelectList skills { get; set; }
+        public string skill { get; set; }
+        public string SearchString { get; set; }
+    }
+}
